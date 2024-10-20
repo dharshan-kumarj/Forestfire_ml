d19dd578f0d11d167e7385b84d718860d93cb55f
dharshan-kumarj <jdharshankumar18@gmail.com>
2024-10-20 21:48:23 +0530

    added CORS

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 # main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api import router
 
 app = FastAPI()
+
+# Add CORS middleware to allow cross-origin requests
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can specify certain domains instead of "*" for production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8004, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
